3daf4eb1615cdd4c0e187b451307aba28fb326a6
FIRST_NAME LAST_NAME <mdshoyan30@gmail.com>
2025-02-24 14:39:03 +0600

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Router/Routes.jsx'
-=======
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx'; // Ensure this path is correct
->>>>>>> 35d0c1dbde8a275bf11ede2039a42b87ae089533
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
